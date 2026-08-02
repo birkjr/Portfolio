6affc688de151bbce4991c699fc998368af829f8
Birk Jonathan Ramstad <birk.ramstad@disruptive-technologies.com>
2026-08-02 21:08:03 +0200

    next config error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "."),
   /* config options here */
 };
 
